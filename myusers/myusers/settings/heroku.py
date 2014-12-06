e54294955e4305b8e85dc53941310e965e9b7303
John Stockbauer <stockbauer@gmail.com>
2014-12-06 13:03:29 -0800

    heroku settings change

--- myusers/myusers/settings/heroku.py
+++ myusers/myusers/settings/heroku.py
@@ -14,10 +14,11 @@ STATIC_ROOT = 'staticfiles'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 # settings for heroku
 import dj_database_url
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'dj_database_url.config()',
     }
 }
+"""
 DATABASES['default'] = dj_database_url.config()
